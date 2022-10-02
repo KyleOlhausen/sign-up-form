8a5fd4386dccde5b459992baa8127364ed548041
KyleOlhausen <olhausen.kyle43@gmail.com>
2022-10-01 19:48:57 -0500

    finished validation for first name and last name and added error messages. Changed confirm password to input type password instead of text.

---
+++ main.js
@@ -0,0 +1,58 @@
+const fName = document.querySelector('#first_name');
+const fNameError = document.querySelector('#first_name_error');
+
+const lName = document.querySelector('#last_name');
+const lNameError = document.querySelector('#last_name_error');
+const email = document.querySelector('#user_email');
+const emailError = document.querySelector('#user_email_error');
+const phone = document.querySelector('phone_number');
+const phoneError = document.querySelector('phone_number_error')
+const pw = document.querySelector('#user_password');
+const pwError = document.querySelector('#user_password_error');
+const confirm_pw = document.querySelector('#confirm_pw');
+const confirmPwError = document.querySelector('#confirm_pw_error');
+
+
+
+
+
+fName.addEventListener("input", () => {
+    if(fName.value === ""){
+        fNameError.textContent = "*Please enter your first name";
+    }
+    else{
+        fNameError.textContent = "";
+    }
+});
+
+
+
+lName.addEventListener("input", () => {
+    if(lName.value === ""){
+        lNameError.textContent = "*Please enter your last name";
+    }
+    else{
+        lNameError.textContent = "";
+    }
+});
+
+email.addEventListener("input", () => {
+    if(email.value === ""){
+        emailError.textContent = "*Please enter your email";
+    }
+    else{
+        emailError.textContent = "";
+    }
+});
+
+
+
+confirm_pw.addEventListener("input", () => {
+    if(confirm_pw.value != pw.value)
+    {
+        confirmPwError.textContent = "*Passwords must match";
+    }
+    else{
+        confirmPwError.textContent = "";
+    }
+});
